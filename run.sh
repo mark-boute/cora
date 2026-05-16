9d8a55a01f2f32fec4c2b8527ffd3d38fe425bff
Mark Boute <bmark0702@gmail.com>
2026-05-16 23:53:14 +0200

    helper

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ run_file() {
     printf "${BLUE_BOLD}Running:${NC} %s\n" "$1"
     
     start=$(date +%s%3N)
-    output=$(gradle run -q --args="../$1")
+    output=$(gradle run --args="../$1")
     end=$(date +%s%3N)
     printf "%s\n" "$output"
     result=$(echo "$output" | head -n 1)
